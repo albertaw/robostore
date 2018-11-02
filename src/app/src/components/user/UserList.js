7ecc87b8aa995430d382cb5144ce977e98e2bb54
Alberta Williams <alberta04@gmail.com>
2018-11-02 11:55:49 -0400

    add links to userlist

--- src/app/src/components/user/UserList.js
+++ src/app/src/components/user/UserList.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 function UserList(props) {
-	let userItems = props.users.map((user) =>
-		<li>{user.name}</li>
-	);
+	let userItems = props.users.map(function(user){
+		let name = user.name.replace(/\s/g, '');
+		const path = 'users/' + name;
+		return (
+			<li>
+				<Link to={path}>{user.name}</Link>
+			</li>
+		)
+	});
 
 	return (
 		<ul>{userItems}</ul>
