76ff3f46ec9f3c2c6d8155f819a08d6103a16e0c
Alberta Williams <alberta04@gmail.com>
2018-11-03 21:29:09 -0400

    add sellers to item detail page

--- app/src/pages/ItemDetailPage.js
+++ app/src/pages/ItemDetailPage.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import App from '../App';
+import UserList from '../components/user/UserList';
 
-function ItemDetailPage({match}) {
-	return (
-		<App>
-			<h1>I am the item {match.params.id} page</h1>
-		</App>
-	)
+class ItemDetailPage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			sellers: []
+		}
+	}
+
+	componentDidMount() {
+		const path = 'http://localhost:4000/api/items/' + this.props.match.params.id + '/users';
+
+		axios.get(path)
+			.then(response => this.setState({sellers: response.data}));
+			console.log(this.props.match.params.id)
+	}
+
+	render() {
+		return (
+			<App>
+				<h2>I am the item {this.props.match.params.id} page</h2>
+				<h3>Sellers</h3>
+				<UserList users={this.state.sellers} />
+			</App>
+		)
+	}
 }
 
 export default ItemDetailPage;
